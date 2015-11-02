a78c4dd48d0cc5532e329dce03d3a3d763ad11fc
darcectech <darylcecile@Gmail.com>
2015-11-02 23:55:47 +0000

    NB Varvar changes

--- NBLib/NB VarVar/varvar.js
+++ NBLib/NB VarVar/varvar.js
@@ -1,6 +1,5 @@
 //NOTE: In order to use this library, you will need to ensure you have jQuery on your HTML page.
 $(document).ready(function(){
-	alert('syncing0');
 	syncVariable("box");
 });
 
@@ -12,10 +11,10 @@ function syncVariable (varToFind) {
 		url: 'var.xml' ,
 		dataType: "xml" ,
 		success: function(xml) {
-			alert('finding');
 			var xmlDoc = $.parseXML( xml );   
 			$(xmlDoc).find(varToFind).each(function(){
-				nm= $(this).text()
+				nm= $(this).text();
+				alert(nm);
 				$("div.NB-"+varToFind).html(nm);
 			})
 		}
