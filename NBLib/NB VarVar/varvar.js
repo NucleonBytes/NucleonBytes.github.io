049c30e366d02fb7f479b15efd25947cf54ee996
darcectech <darcectech@users.noreply.github.com>
2015-11-03 12:42:22 +0000

    Update varvar.js

--- NBLib/NB VarVar/varvar.js
+++ NBLib/NB VarVar/varvar.js
@@ -1,15 +1,29 @@
 //NOTE: In order to use this library, you will need to ensure you have jQuery on your HTML page.
+$(document).ready(function(){
+  var all = document.getElementsByTagName("*");
+  for (var i=0, max=all.length; i < max; i++) {
+    if(all[i].className.indexOf("NBV-") > -1){
+      syncVar(all[i].className.split('-')[1]);
+    }
+  }
+});
 
 function syncVar(varToFind){
 	var txtFile = new XMLHttpRequest();
-	txtFile.open("GET", "https://nucleonbytes.github.io/NBLib/NB%20VarVar/var.xml", true);
+	txtFile.open("GET", "https://nucleonbytes.github.io/NBLib/NB%20VarVar/var.xml", true);//https://nucleonbytes.github.io/NBLib/NB%20VarVar/
 	txtFile.onreadystatechange = function() {
   		if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
     		if (txtFile.status === 200) {  // Makes sure it's found the file.
     			var allText = txtFile.responseText; 
     	  		var xmlDoc = $.parseXML( allText );
-    	  		var xx = $(xmlDoc).find('NucleonBytesVariables').find(varToFind).first().html(); //changed text to html
-    	  		$(".NBV-"+varToFind).html(xx);
+    	  		var xx = $(xmlDoc).find('NucleonBytesVariables').find(varToFind).first().html();
+            if (xx==""){
+              //No match found or match is empty so ignore
+            }
+            else
+            {
+              $(".NBV-"+varToFind).html(xx);
+            }
   			}
 		}
 	}
