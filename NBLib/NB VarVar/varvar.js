00deaa951452b8ee74658aceb6efcf57032eb3e6
darcectech <darylcecile@Gmail.com>
2015-11-03 00:30:40 +0000

    edit7

--- NBLib/NB VarVar/varvar.js
+++ NBLib/NB VarVar/varvar.js
@@ -1,24 +1,22 @@
 //NOTE: In order to use this library, you will need to ensure you have jQuery on your HTML page.
 $(document).ready(function(){
-	syncVariable("box");
+	syncVar("box");
 });
 
-function syncVariable (varToFind) {
-	var nm;
-	alert('syncing');
-	$.ajax({
-		type: "GET" ,
-		url: 'https://nucleonbytes.github.io/NBLib/NB%20VarVar/var.xml' ,
-		dataType: "xml" ,
-		success: function(xml) {
-			alert('testSTART');
-			var xmlDoc = $.parseXML( xml );
-			alert(xmlDoc);   
-			$(xmlDoc).find(varToFind).each(function(){
-				alert($(this).text());
-				nm= $(this).text();
-				$(".NB-var").html(nm);
-			})
+function syncVar(varToFind){
+	var txtFile = new XMLHttpRequest();
+	txtFile.open("GET", "https://nucleonbytes.github.io/NBLib/NB%20VarVar/var.xml", true);
+	txtFile.onreadystatechange = function() {
+  		if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    		if (txtFile.status === 200) {  // Makes sure it's found the file.
+    			alert("reading");
+    			var allText = txtFile.responseText; 
+    	  		var xmlDoc = $.parseXML( allText );
+    	  		var xx = $(xmlDoc).find('var').find('box').first().text();
+    	  		alert(xx);
+    	  		$(".NB-var").html(txtFile.responseText);
+  			}
 		}
-	});
+	}
+	txtFile.send(null);
 }
