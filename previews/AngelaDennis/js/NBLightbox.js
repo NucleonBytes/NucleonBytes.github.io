bfb3fb812950ff85971f1022bc2afbb26ad11778
darcectech <darylcecile@Gmail.com>
2015-11-08 22:17:07 +0000

    Update
    Fixed lightbox resize bug

--- previews/AngelaDennis/js/NBLightbox.js
+++ previews/AngelaDennis/js/NBLightbox.js
@@ -1,4 +1,4 @@
-$('.row').on('click', '#lbtrigger' ,function() { 
+/*$('.row').on('click', '#lbtrigger' ,function() {
 	var src = $(this).find('img').attr('src');
 	$('.lightbox').css('display','block');
 	$('.lightboxdisplay').attr('src', src);
@@ -13,9 +13,24 @@ $('.row').on('click', '#lbtrigger' ,function() {
 	var newY = (vpHeight/2)-(imageHeight/2);
 	$('.lightboxdisplay').css('left',newX+"px");
 	$('.lightboxdisplay').css('top',newY+"px");
+});*/
+
+$('.row').on('click', '#lbtrigger' ,function() {
+	var src = $(this).find('img').attr('src');
+	$('.lightbox').css('display','block');
+	$('.lightboxdisplay').attr('src', src);
+	$('.lightboxdisplay').css('display','block');
+	var imageWidth = $('.lightboxdisplay').width();
+	var imageHeight = $('.lightboxdisplay').height();
+	var vpWidth = $(window).width();
+	var vpHeight = $(window).height();
+	var newX = (vpWidth/2)-(imageWidth/2);
+	var newY = (vpHeight/2)-(imageHeight/2);
+	$('.lightboxdisplay').css('left',newX+"px");
+	$('.lightboxdisplay').css('top',newY+"px");
 });
 
-$('.lightbox').click( function() { 
+$('.lightbox').click( function() {
 	$('.lightbox').css('display','none');
 	$('.lightboxdisplay').css('display','none');
 })
