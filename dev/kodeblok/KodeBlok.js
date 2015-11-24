919c294673f9a81065022e8cbb6819c1181cb52d
darcectech <darylcecile@Gmail.com>
2015-11-24 22:24:12 +0000

    kodeblok m10
    minor

--- dev/kodeblok/KodeBlok.js
+++ dev/kodeblok/KodeBlok.js
@@ -13,8 +13,8 @@ function readfile(){
   		if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
     		if (txtFile.status === 200) {  // Makes sure it's found the file.
     			thetext = txtFile.responseText;
-    			var resu = thetext.match(/[#_]*(.*)(.|\r|\n)*?![\n|#]?"/g);
-    			alert("hi");
+    			var resu = thetext.match(/[#_]*(.*)(.|\r|\n)*?![\n|#]?/g);
+    			alert("hix");
     			alert(resu);
   			}
   			else
