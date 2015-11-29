255caff3103a24a8d32997ae503f84e150aa4d52
darcectech <darylcecile@Gmail.com>
2015-11-29 16:52:27 +0000

    minor
    Fixes mobile view bugs

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ $(document).ready(function() {
   $('body').css('display', 'none');
   $('body').fadeIn(1000);
 
+  if ($(window).width() < 900){
+    $("#navhome").html('<i class="material-icons">home</i>');
+    $("#navplans").html('<i class="material-icons">local_atm</i>');
+    $("#navpreview").html('<i class="material-icons">ondemand_video</i>');
+    $("#navcontact").html('<i class="material-icons">touch_app</i>');
+  }
+  else {
+    $("#navhome").html('HOME');
+    $("#navplans").html('PLANS');
+    $("#navpreview").html('PREVIEW');
+    $("#navcontact").html('GET IN TOUCH');
+  }
+
   $('nav ul li a').click(function() {
     event.preventDefault();
     newLocation = this.href;
@@ -19,6 +32,13 @@ $(document).ready(function() {
       $('body').fadeOut(1000, newpage);
     }
   });
+
+  $('.handle').on('click', function() {
+  		$('nav ul').toggleClass('autoShow');
+  	})
+  $('nav ul li').on('click', function(){
+  	$('nav ul').toggleClass('autoShow');
+  })
 
   if ($(window).width() <988){
     $(".col33").removeClass("paddright2");
@@ -35,6 +55,19 @@ $(document).ready(function() {
         $(".col33").addClass("paddright2");
         $(".col33").addClass("paddleft2");
       }
+    }
+
+    if ($(window).width() < 900){
+      $("#navhome").html('<i class="material-icons">apps</i>');
+      $("#navplans").html('<i class="material-icons">local_atm</i>');
+      $("#navpreview").html('<i class="material-icons">ondemand_video</i>');
+      $("#navcontact").html('<i class="material-icons">touch_app</i>');
+    }
+    else {
+      $("#navhome").html('HOME');
+      $("#navplans").html('PLANS');
+      $("#navpreview").html('PREVIEW');
+      $("#navcontact").html('GET IN TOUCH');
     }
   });
 
