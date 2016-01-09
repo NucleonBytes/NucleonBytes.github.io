953b2b0a1d05af57e05a67dc7077e390e42f929b
darcectech <darylcecile@Gmail.com>
2016-01-09 23:46:16 +0000

    bs

--- byteSearch/action.js
+++ byteSearch/action.js
@@ -2,7 +2,7 @@
 var currentSearch = 0;
 var pageCount = 1;
 var currentTerm = "";
-    var intv;
+var intv;
 
 $(document).on("ready", function () {
     $('#searchbox').keyup(function () {
@@ -16,19 +16,28 @@ $(document).on("ready", function () {
         }
     });
     
+    var xChecker = setInterval(function(){
+        if ($(".stayPanel .title").text() == "Title"){
+            $(".stayPanel").css("opacity",0);
+        }
+        checkWiki();
+    },100);
+
 
     $(document).on('mouseenter', '.resultblock', function (event) {
         var link = $(this).find(".title").attr("href");
-        
-        intv = setInterval(function(){
+
+        intv = setInterval(function () {
             pollImageReady(getHost(link).replace(/.*?:\/\//g, ""));
-        },1000);
-        
-        $(".stay").animate({opacity:1},700);
+        }, 2000);
+
+        $(".stay").animate({ opacity: 1 }, 700);
+        $(".stayPanel").animate({ opacity: 0 }, 700);
     }).on('mouseleave', '.resultblock', function () {
         clearInterval(intv);
-        $(".stay").animate({opacity:0},400);
-        $(".stay").attr("src","https://promotions.coca-cola.com/etc/designs/promotions/img/loading.gif")
+        $(".stay").animate({ opacity: 0 }, 400);
+        $(".stayPanel").animate({ opacity: 1 }, 400);
+        $(".stay").attr("src", "https://promotions.coca-cola.com/etc/designs/promotions/img/loading.gif")
     });
 
     if (document.location.search.length) {
@@ -36,31 +45,34 @@ $(document).on("ready", function () {
         var terms = $("#searchbox").val();
         $(".resultBox").empty();
         pageCount = 1;
+        
+        $(".stayPanel .title").empty();
+        $(".stayPanel .panelPeek").attr("src","");
+        $(".stayPanel .desc").empty();
         parseResultsXML(terms, 0);
     } else {
         // no query string exists
     }
 });
 
-function pollImageReady(url){
-    var site = "http://free.pagepeeker.com/v2/thumbs_ready.php?size=m&url="+url;    
-    $.getJSON(site,function(data){
+function pollImageReady(url) {
+    var site = "http://free.pagepeeker.com/v2/thumbs_ready.php?size=m&url=" + url;
+    $.getJSON(site, function (data) {
         var obj = JSON.parse(JSON.stringify(data));
         var val = obj.IsReady;
-        if (val == 1){
+        if (val == 1) {
             clearInterval(intv);
-            $(".stay").attr("src","http://api3.pagepeeker.com/v2/thumbs.php?size=x&url="+url);
+            $(".stay").attr("src", "http://api3.pagepeeker.com/v2/thumbs.php?size=x&url=" + url);
         }
-        else
-        {
+        else {
             console.log(val);
-            $(".stay").attr("src","https://promotions.coca-cola.com/etc/designs/promotions/img/loading.gif")
+            $(".stay").attr("src", "https://promotions.coca-cola.com/etc/designs/promotions/img/loading.gif")
         }
     });
 }
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 20) {
         if (currentTerm != "") {
             $(".resultBox").append("<b style='text-align:center'> ~~~ Page " + pageCount + " ~~~ </b>")
             pageCount += 1;
@@ -104,7 +116,14 @@ function filterKeyPress(e) {
         var terms = $("#searchbox").val();
         $(".resultBox").empty();
         pageCount = 1;
+        
+        $(".stayPanel .title").empty();
+        $(".stayPanel .panelPeek").attr("src","");
+        $(".stayPanel .desc").empty();
+        
         parseResultsXML(terms, 0);
+        
+        
     }
 }
 
@@ -137,20 +156,66 @@ function called(itm) {
     formJResults(JSON.stringify(itm));
 }
 
+function checkWiki() {
+    var firstResult = $(document).find(".resultblock");
+    var firstAddress = firstResult.find(".root").html();
+    var firstheading = firstResult.find(".title").html();
+    var firstImage = firstResult.find(".hidden").text();
+    var firstdesc = firstResult.find(".desc").text().replace(/\n/gm, '');
+    
+    if (firstAddress.toLowerCase().indexOf("wikipedia.org") >= 0) {
+        var imgLoc = firstImage.replace('"','').split('""')[0].replace('"','');
+        showPanel(imgLoc,firstdesc,firstheading,firstAddress);
+    }
+    else
+    {
+        $(".stayPanel").css("opacity",0); 
+        $(".stayPanel .title").empty();
+        $(".stayPanel .panelPeek").attr("src","");
+        $(".stayPanel .desc").empty();
+    }
+}
+
+String.prototype.trunc =
+     function( n, useWordBoundary ){
+         var isTooLong = this.length > n,
+             s_ = isTooLong ? this.substr(0,n-1) : this;
+         s_ = (useWordBoundary && isTooLong) ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
+         return  isTooLong ? s_ + '...' : s_;
+      };
+
+function showPanel(imgLoc, desc, heading, link){
+    var panel_image = $(".stayPanel").find(".panelPeek");
+    var panel_title = $(".stayPanel").find(".title");
+    var panel_description = $(".stayPanel").find(".desc");
+    panel_image.attr("src",imgLoc);
+    panel_title.html(heading);
+    panel_description.html(desc.trunc(435,true)+ " <a href=" + link+ "> Learn more </a>");
+    
+    $(".stayPanel").css("opacity",1);
+}
+
 function formJResults(jsonS) {
     var $json = $.parseJSON(jsonS);
     $.each($json.results, function (idx, obj) {
         var linc = JSON.stringify($json.results[idx].unescapedUrl);
         var titl = JSON.stringify($json.results[idx].title);
         var desc = JSON.stringify($json.results[idx].content);
-        createJResultBlock(titl, linc, desc);
+        var cseImage = "";
+        if (linc.toLowerCase().indexOf("wikipedia.org") >= 0) {
+            cseImage = JSON.stringify($json.results[idx].richSnippet.cseImage.src);
+        }
+        createJResultBlock(titl, linc, desc, cseImage);
     });
+    checkWiki();
 }
 
-function createJResultBlock(xtitle, path, desc) {
+function createJResultBlock(xtitle, path, desc, zimg) {
     path = path.replace(/^"(.+(?="$))"$/, '$1');
     xtitle = xtitle.replace(/^"(.+(?="$))"$/, '$1');
     desc = desc.replace(/^"(.+(?="$))"$/, '$1');
+
+    var isWiki = (path.toLowerCase().indexOf("wikipedia.org") >= 0);
 
     var resBlock = document.createElement("div");
     resBlock.className = "resultblock";
@@ -172,6 +237,15 @@ function createJResultBlock(xtitle, path, desc) {
     xdesc.innerHTML = desc;
     resBlock.appendChild(xdesc);
 
+    if (isWiki == true) {
+        zimg = zimg || "";
+
+        var zimage = document.createElement("p")
+        zimage.className = "hidden";
+        zimage.innerText = String(zimg).replace('""','');
+        resBlock.appendChild(zimage);
+    }
+
     var mimg = document.createElement("img");
     mimg.src = "http://www.google.com/s2/favicons?domain=" + getHost(path);
     resBlock.appendChild(mimg);
@@ -188,37 +262,38 @@ function formResults(xmlS) {
             var addr = $(this).find("id").text();
             var titl = $(this).find("title").text();
             var desc = $(this).find("summary").text();
-            createResultBlock(titl, addr, desc);
+            var cseImage = "";
+            createJResultBlock(titl, addr, desc, cseImage);
         }
     });
-    currentStartAt += 10;
+    checkWiki();
 }
 
-function createResultBlock(xtitle, path, desc) {
-    var resBlock = document.createElement("div");
-    resBlock.className = "resultblock";
-    $(".resultBox").append(resBlock);
+// function createResultBlock(xtitle, path, desc) {
+//     var resBlock = document.createElement("div");
+//     resBlock.className = "resultblock";
+//     $(".resultBox").append(resBlock);
 
-    var title = document.createElement("a");
-    title.className = "title";
-    title.innerHTML = xtitle;
-    title.href = path;
-    resBlock.appendChild(title);
+//     var title = document.createElement("a");
+//     title.className = "title";
+//     title.innerHTML = xtitle;
+//     title.href = path;
+//     resBlock.appendChild(title);
 
-    var xroot = document.createElement("p");
-    xroot.className = "root";
-    xroot.innerHTML = getHost(path);
-    resBlock.appendChild(xroot);
+//     var xroot = document.createElement("p");
+//     xroot.className = "root";
+//     xroot.innerHTML = getHost(path);
+//     resBlock.appendChild(xroot);
 
-    var xdesc = document.createElement("p");
-    xdesc.className = "desc";
-    xdesc.innerHTML = desc;
-    resBlock.appendChild(xdesc);
+//     var xdesc = document.createElement("p");
+//     xdesc.className = "desc";
+//     xdesc.innerHTML = desc;
+//     resBlock.appendChild(xdesc);
 
-    var mimg = document.createElement("img");
-    mimg.src = "http://www.google.com/s2/favicons?domain=" + getHost(path);
-    resBlock.appendChild(mimg);
-}
+//     var mimg = document.createElement("img");
+//     mimg.src = "http://www.google.com/s2/favicons?domain=" + getHost(path);
+//     resBlock.appendChild(mimg);
+// }
 
 function getHost(address) {
     var att = document.createElement('a');
