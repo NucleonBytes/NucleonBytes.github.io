2d32cad483b9df69d63eabfaa3f4d3256a01dfd0
darcectech <darylcecile@Gmail.com>
2016-01-23 14:14:17 +0000

    fgvt

--- previews/FaithValleyGroup/actions.js
+++ previews/FaithValleyGroup/actions.js
@@ -1,14 +1,31 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
     var height = $(window).scrollTop();
-    if (height > 100){
-        if ( $("nav").hasClass("drop") == false ){
+    if (height > 100) {
+        if ($("nav").hasClass("drop") == false) {
             $("nav").addClass("drop")
         }
     }
-    else
-    {
-        if ( $("nav").hasClass("drop") == true ){
+    else {
+        if ($("nav").hasClass("drop") == true) {
             $("nav").removeClass("drop")
         }
     }
 });
+
+$(function () {
+    $(".lightbox").on("click", function () {
+        $(".lightbox").animate({
+            opacity:0
+        },600);
+        $(".lightbox").toggleClass("hidden");
+    });
+    
+    $(".gal").on("click", function() {
+        $(".lightbox").toggleClass("hidden");
+        $(".lightbox").animate({
+            opacity:1
+        },600);
+        $(".lightbox img.master").attr("src",$(this).attr("src"))
+    });
+});
+
