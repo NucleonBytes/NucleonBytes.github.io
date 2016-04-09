52efd36afbab3427c5042fdeb526633c885a2686
darcectech <darylcecile@Gmail.com>
2016-04-09 13:10:20 +0100

    minor
    conflic resolver post

--- previews/FaithValleyGroup/Lib/nucleon.js
+++ previews/FaithValleyGroup/Lib/nucleon.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 $(function () {
     console.log("running...");
     updateComponent();
@@ -39,46 +37,4 @@ function startRoller(){
         }
         $("#nucleon-carousel div")[positionVal].setAttribute("Class","active");
     }, 6000);
-=======
-
-$(function () {
-    console.log("running...");
-    updateComponent();
-    startRoller();
-
-    var currurl = window.location.pathname;
-    var index = currurl.lastIndexOf("/") + 1;
-    var filename = currurl.substr(index);
-});
-
-$(window).resize(function() {
-    updateComponent();
-});
-
-function updateComponent(){
-    var finalHeight = ($(window).height()/100)*45;
-    $("#nucleon-carousel").height(finalHeight);
-}
-
-var positionVal = 0;
-var imageCount = 0;
-function startRoller(){
-    imageCount = $("#nucleon-carousel div").length;
-    positionVal = imageCount-1;
-    var interv = setInterval(function(){
-        console.log(positionVal);
-        if (positionVal == -1){
-            clearInterval(interv);
-            return;
-        }
-        $("#nucleon-carousel div")[positionVal].setAttribute("Class","inactive");
-        if (positionVal - 1 <0){
-            positionVal = imageCount-1;
-        }
-        else{
-            positionVal-=1;
-        }
-        $("#nucleon-carousel div")[positionVal].setAttribute("Class","active");
-    }, 6000);
->>>>>>> origin/master
 }
--- previews/FaithValleyGroup/actions.js
+++ previews/FaithValleyGroup/actions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(window).scroll(function () {
     var height = $(window).scrollTop();
     if (height > 130) {
@@ -37,7 +36,7 @@ $(function () {
         {
             $(".lightbox img.master").attr("id","");
         }
-        
+
         $(".lightbox img.master").attr("src",$(this).attr("src"));
     });
 });
@@ -50,56 +49,3 @@ function checkOrients(img){
         }
     });
 }
-=======
-$(window).scroll(function () {
-    var height = $(window).scrollTop();
-    if (height > 130) {
-        if ($("nav").hasClass("drop") == false) {
-            $("nav").addClass("drop")
-        }
-    }
-    else {
-        if ($("nav").hasClass("drop") == true) {
-            $("nav").removeClass("drop")
-        }
-    }
-});
-
-$(function () {
-    $(".lightbox").on("click", function () {
-        $(".lightbox").animate({
-            opacity:0
-        },600);
-        $(".lightbox").toggleClass("hidden");
-    });
-    
-    $(".gal").on("click", function() {
-        checkOrients(this);
-        $(".lightbox").toggleClass("hidden");
-        $(".lightbox").animate({
-            opacity:1
-        },600);
-        $(".gal").attr("opacity","0");
-        $(".lightbox img.master").attr("src",$(this).attr("src"));
-        
-        var attr = $(this).attr('id');
-        if (typeof attr !== typeof undefined && attr !== false) {
-            $(".lightbox img.master").attr("id",$(this).attr("id"));
-        }
-        else
-        {
-            $(".lightbox img.master").attr("id","");
-        }
-    });
-});
-
-function checkOrients(img){
-    EXIF.getData(img, function(){
-        var myOri = EXIF.getTag(this, "Orientation").toString();
-        if (myOri == "6"){
-            //rotate90
-            $(img).attr("id","rotate90");
-        }
-    });
-}
->>>>>>> origin/master
