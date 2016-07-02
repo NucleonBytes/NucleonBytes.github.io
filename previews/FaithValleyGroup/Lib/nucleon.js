4a958074e7676c94e0f99e05d81e3b5c3da48060
darcectech <darylcecile@Gmail.com>
2016-07-02 15:06:05 +0100

    improved the banner nucleon

--- previews/FaithValleyGroup/Lib/nucleon.js
+++ previews/FaithValleyGroup/Lib/nucleon.js
@@ -1,7 +1,15 @@
 $(function () {
-    console.log("running...");
     updateComponent();
     startRoller();
+
+    var currurl = window.location.pathname;
+    var index = currurl.lastIndexOf("/") + 1;
+    var filename = currurl.substr(index);
+});
+
+$(function(){
+    updateComponent();
+    startRollerX();
 
     var currurl = window.location.pathname;
     var index = currurl.lastIndexOf("/") + 1;
@@ -41,4 +49,67 @@ function startRoller(){
         }
         $("#nucleon-carousel div")[positionVal].setAttribute("Class","active");
     }, 6000);
+}
+
+function startRollerX(){
+    var interv = setInterval(function(){
+        var al = $("#nucleon-banner div");
+        var ali = [];
+        for (var i = 0; i < $("#nucleon-banner div").length; i++) {
+            if ($("#nucleon-banner div")[i].getAttribute("Class") == "inactive"){
+                ali.push("i");
+            }
+            else{
+                ali.push("a");
+            }
+        }
+        var nali = getPatternNext(ali);
+        var nextToBack;
+        for (var i = 0; i < nali.length; i++) {
+            if (nali[i]=="a"){
+                $("#nucleon-banner div")[i].setAttribute("Class","active");
+            }
+            else{
+                $("#nucleon-banner div")[i].setAttribute("Class","inactive");
+                nextToBack = $("#nucleon-banner div")[i];
+            }
+        }
+        setTimeout(function(){
+            $("#nucleon-banner")[0].appendChild(nextToBack);
+        },3000);
+    }, 6000);
+}
+
+function getPatternNext(currentA){
+    // [a,a,a,i,i] => [i,a,a,a,i] => [i,i,a,a,a] => [a,i,i,a,a]
+    var wid = $('#nucleon-banner').width();
+    if (wid<=760){
+        for (var i = 0; i < currentA.length; i++) {
+            currentA[i] = "i";
+        }
+        currentA[currentA.length-1] = "a";
+    }
+    else{
+        if (countOccurence(currentA,"a")==1){
+            for (var i = 0; i < currentA.length; i++) {
+                currentA[i] = "i";
+            }
+            currentA[currentA.length-3] = "a";
+            currentA[currentA.length-2] = "a";
+            currentA[currentA.length-1] = "a";
+        }
+    }
+    var l = currentA.pop();
+    currentA.unshift(l);
+    return currentA;
+}
+
+function countOccurence(arr,itm){
+    var counts = {};
+    
+    for(var i = 0; i< arr.length; i++) {
+        var num = arr[i];
+        counts[num] = counts[num] ? counts[num]+1 : 1;
+    }
+    return counts[itm];
 }
