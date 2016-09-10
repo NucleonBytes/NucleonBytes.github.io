669d0ecf1d4e4b8840eb22463e525abc2bde328d
darcectech <darylcecile@gmail.com>
2016-09-10 16:52:04 +0100

    moi web

---
+++ previews/MoizaBertrandX/script.js
@@ -0,0 +1,21 @@
+setInterval(function(){
+	if ($('body')[0].scrollTop >= 350){
+		showSecondImage();
+	}
+	if ($('body')[0].scrollTop < 350){
+		showFirstImage();
+	}
+},500);
+
+
+// IMAGE TRANSITION
+
+function showFirstImage(){
+	$('.bgImage#p2').animate({'opacity':'0'},500);
+	$('p.content').animate({'opacity':'0'});
+}
+
+function showSecondImage(){
+	$('.bgImage#p2').animate({'opacity':'1'},500);
+	$('p.content').animate({'opacity':'1'});
+}
